b3ba138589de288465bfd69fb4eec879ff967d12
NikiforukDM <109328955+NikiforukDM@users.noreply.github.com>
2022-07-18 16:22:31 +0300

    Add files via upload

---
+++ ДЗ6.py
@@ -0,0 +1,203 @@
+import re
+from normalize import normalize
+import file_parser as parser
+import shutil
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MOV_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+TXT_DOCUMENTS = []
+PDF_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+IMAGES = [JPEG_IMAGES, JPG_IMAGES, PNG_IMAGES, SVG_IMAGES]
+VIDEO = [AVI_VIDEO, MP4_VIDEO, MOV_VIDEO, MKV_VIDEO]
+DOCUMENTS = [DOC_DOCUMENTS, DOCX_DOCUMENTS, TXT_DOCUMENTS,
+             PDF_DOCUMENTS, XLSX_DOCUMENTS, PPTX_DOCUMENTS]
+AUDIO = [MP3_AUDIO, OGG_AUDIO, WAV_AUDIO, AMR_AUDIO]
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
+    'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'ZIP': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        ext = get_extension(item.name)
+        fullname = folder / item.name
+        if not ext:
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+
+
+if __name__ == '__main__':
+
+    folder_for_scan = sys.argv[1]
+    print(f'Start in folder {folder_for_scan}')
+
+    scan(Path(folder_for_scan))
+    print(f'Images: {IMAGES}')
+    print(f'Video: {VIDEO}')
+    print(f'Audio: {AUDIO}')
+    print(f'Documents: {DOCUMENTS}')
+    print(f'Archives: {ARCHIVES}')
+    print(f'Types of files in folder: {EXTENSIONS}')
+    print(f'Unknown files of types: {UNKNOWN}')
+
+    print(FOLDERS[::-1])
+
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    return t_name
+
+
+def handle_media(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_other(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_archive(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / \
+        normalize(filename.name.replace(filename.suffix, ''))
+
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()),
+                              str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'Це не архів {filename}!')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+
+def handle_folder(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Помилка видалення папки {folder}')
+
+
+def main(folder: Path):
+    parser.scan(folder)
+    for file in parser.JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    for file in parser.JPG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPG')
+    for file in parser.PNG_IMAGES:
+        handle_media(file, folder / 'images' / 'PNG')
+    for file in parser.SVG_IMAGES:
+        handle_media(file, folder / 'images' / 'SVG')
+    for file in parser.AVI_VIDEO:
+        handle_media(file, folder / 'video' / 'AVI')
+    for file in parser.MP4_VIDEO:
+        handle_media(file, folder / 'video' / 'MP4')
+    for file in parser.MOV_VIDEO:
+        handle_media(file, folder / 'video' / 'MOV')
+    for file in parser.MKV_VIDEO:
+        handle_media(file, folder / 'video' / 'MKV')
+    for file in parser.DOC_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'DOC')
+    for file in parser.DOCX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'DOCX')
+    for file in parser.TXT_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'TXT')
+    for file in parser.PDF_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'PDF')
+    for file in parser.XLSX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'XLSX')
+    for file in parser.PPTX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'PPTX')
+    for file in parser.MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3')
+    for file in parser.OGG_AUDIO:
+        handle_media(file, folder / 'audio' / 'OGG')
+    for file in parser.WAV_AUDIO:
+        handle_media(file, folder / 'audio' / 'WAV')
+    for file in parser.AMR_AUDIO:
+        handle_media(file, folder / 'audio' / 'AMR')
+    for file in parser.MY_OTHER:
+        handle_other(file, folder / 'MY_OTHER')
+    for file in parser.ARCHIVES:
+        handle_archive(file, folder / 'archives')
+
+    for folder in parser.FOLDERS[::-1]:
+        handle_folder(folder)
+
+
+if __name__ == '__main__':
+    if sys.argv[1]:
+        folder_for_scan = Path(sys.argv[1])
+        print(f'Start in folder {folder_for_scan.resolve()}')
+        main(folder_for_scan.resolve())
